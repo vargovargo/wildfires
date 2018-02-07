95ba94f161d569b7565a18132ca3da1c92c4721b
vargo <jvargo@gmail.com>
2018-02-06 21:07:21 -0800

    correcting wildfire Figs

--- mapMatrix.R
+++ mapMatrix.R
@@ -3,7 +3,6 @@ rm(list = ls())
 library(ncdf4)
 library(tidyverse)
 library(data.table)
-
 setwd("~/Wildfires/")
 # retrieve a list of nc files in my data folder:
 
@@ -228,7 +227,7 @@ matrix %>%
 
 POP2000 <- as.data.table(read.csv("./data/Intercensal_2000-2010_DBInput_csv.txt", header=T, stringsAsFactors = F)) %>%
   mutate(ageCat = ifelse(Age <=5, "under5",ifelse(Age >=65,"over65","other"))) %>%
-  filter(ageCat != "other" & Year != "4/1/2000 0:00:00" & Year != "4/1/2010 0:00:00" & CountyName != "California") %>% 
+  filter(Year != "4/1/2000 0:00:00" & Year != "4/1/2010 0:00:00" & CountyName != "California") %>% 
   mutate(year = as.numeric(substring(sapply(strsplit(as.character(Year), "/"), "[[", 3),1,4))) %>%
   mutate(county_name = ifelse(CountyName =="ContraCosta", "Contra Costa",
                               ifelse(CountyName =="DelNorte", "Del Norte",
@@ -253,9 +252,8 @@ POP2000 <- as.data.table(read.csv("./data/Intercensal_2000-2010_DBInput_csv.txt"
 
 # bring in Population Projection Data
 POPproj <- as.data.table(read.csv("./data/P3_Complete.csv", header=T)) %>%
-  mutate(ageCat = ifelse(agerc <=5, "under5",ifelse(agerc >=65,"over65","other"))) %>%
-  filter(ageCat != "other") %>%
-  mutate(stcoFIPS = as.numeric(as.character(fips))) %>%
+  mutate(ageCat = ifelse(agerc <=5, "under5",ifelse(agerc >=65,"over65","other")),
+         stcoFIPS = as.numeric(as.character(fips))) %>%
   full_join(read.csv("~/CA_stcoFIPS_key.csv", header=T)) %>%
   group_by(ageCat, year, stcoFIPS, county_name) %>%
   summarise(people = sum(perwt, na.rm=T)) %>%
@@ -268,9 +266,10 @@ POPproj <- as.data.table(read.csv("./data/P3_Complete.csv", header=T)) %>%
   summarise(people = mean(people, na.rm=T)) %>%
   as.data.table() %>% setkeyv("county_name")
 
-
-
-write.csv(POPproj, "./data/StatePOPprojections.csv", row.names = F)
+POPproj %>% spread(key = ageCat, value = people) %>%
+  mutate(totalPop = (other + over65 + under5)) %>% 
+  mutate(pctOld = over65/(totalPop),
+         pctYoung = under5/(totalPop)) %>% write.csv(., "./data/StatePOPprojectionsPCT.csv", row.names = F)
 
 
 POPproj %>%
@@ -289,29 +288,180 @@ POPproj %>%
 ######### results tables
 #####################################
 
-resultsTables <- read.csv("https://raw.githubusercontent.com/vargovargo/wildfires/master/AgeSpecificResults.csv", header=T, stringsAsFactors = F )
+resultsTables <- read.csv("https://raw.githubusercontent.com/vargovargo/wildfires/master/AgeSpecificResults.csv", header=T, stringsAsFactors = F) %>%
+  mutate(class = factor(ifelse(Disease %in% c("Respiratory (all)", 
+                                              "Asthma",
+                                              "Acute Bronchitis",
+                                              "COPD",
+                                              "Pneumonia",
+                                              "Upper Respiratory Infection",
+                                              "Respiratory Symptoms",
+                                              "Bronchitis (not specified)"),"Respiratory",
+                               ifelse(Disease %in% c("Cardiovascular (all)",
+                                                     "Ischemic Heart Disease",
+                                                     "Dysrythmia",
+                                                     "Chronic Heart Failure",
+                                                     "Stroke",
+                                                     "Diseases of the Peripheral Circulation"),"Cardiovascular","Overall")),
+                        levels = c("Overall","Respiratory","Cardiovascular")))
 
 resultsTables %>% 
-  filter(lag != "14-day" & U95 < 10 & age !="all" &
-           Disease %in% c("Respiratory (all)", "Asthma", "Total Encounters", "Acute Bronchitis", "Pneumonia", "Upper Respiratory Infection", "Respiratory Symptoms")) %>%
+  filter(lag == "5-day" & age !="all" &
+           Disease %in% c("Respiratory (all)", "Asthma", "Acute Bronchitis", "Pneumonia", "Upper Respiratory Infection", "Respiratory Symptoms", "COPD", "Bronchitis (not specified)")) %>%
   mutate(RR = as.numeric(as.character(RR)), 
-         age = factor(age, levels= c("Age 0-4","Age 5-17","Age 18+")),
-         lag = factor(lag, levels= c("5-day","8-day","14-day")))%>%
+         Disease = factor(Disease, levels= c("Asthma",
+                                             "Upper Respiratory Infection",
+                                             "Respiratory Symptoms",
+                                             "Acute Bronchitis",
+                                             "Bronchitis (not specified)",
+                                             "Pneumonia",
+                                             "COPD",
+                                             "Respiratory (all)",
+                                             "Cardiovascular (all)",
+                                             "Ischemic Heart Disease",
+                                             "Dysrythmia",
+                                             "Chronic Heart Failure",
+                                             "Stroke",
+                                             "Diseases of the Peripheral Circulation",
+                                             "Total Encounters")),
+         visit=factor(visit, levels = c("ED" , "Inpatient Hospitalizations", "Doctor's Office and Clinic Visits")),
+         age = factor(age, levels= c("Age 0-4","Age 5-17","Age 18+", "all")),
+         lag = factor(lag, levels= c("5-day","8-day","14-day")), 
+         sig = ifelse(U95 < 1 | L95 > 1, "Significant","Not Signficant"))%>%
+  filter(age %in% c("Age 0-4","Age 5-17","Age 18+")) %>%
   ggplot() + 
-  geom_point(aes(x=visit, y=RR,  color=age), position = position_dodge(width = 0.7)) + 
-  geom_errorbar(aes(x=visit, ymax=as.numeric(U95), ymin=as.numeric(L95), color=age), position = position_dodge(width = 0.7)) + 
-  geom_hline(yintercept = 1, linetype="dashed") + facet_grid( Disease ~ lag) + coord_flip()
+  geom_point(aes(x=Disease, y=RR,  color=visit, shape=visit, alpha=sig), size=3,position = position_dodge(width = 0.7)) + 
+  geom_linerange(aes(x=Disease, ymax=as.numeric(U95), ymin=as.numeric(L95), color=visit, alpha=sig), position = position_dodge(width = 0.7)) + 
+  geom_hline(yintercept = 1, linetype="dashed") + 
+  coord_cartesian(ylim=c(0, 3)) + facet_grid(.~age) 
 
 resultsTables %>% 
   filter(age =="all") %>%
   mutate(RR = as.numeric(as.character(RR)), 
-         age = factor(age, levels= c("Age 0-4","Age 5-17","Age 18+")),
-         lag = factor(lag, levels= c("5-day","8-day","14-day")))%>%
+         Disease = factor(Disease, levels= c("Respiratory (all)", 
+                                             "Asthma",
+                                             "Acute Bronchitis",
+                                             "COPD",
+                                             "Pneumonia",
+                                             "Upper Respiratory Infection",
+                                             "Respiratory Symptoms",
+                                             "Bronchitis (not specified)",
+                                             "Cardiovascular (all)",
+                                             "Ischemic Heart Disease",
+                                             "Dysrythmia",
+                                             "Chronic Heart Failure",
+                                             "Stroke",
+                                             "Diseases of the Peripheral Circulation",
+                                             "Total Encounters")),
+         visit=factor(visit, levels = c("ED" , "Inpatient Hospitalizations", "Doctor's Office and Clinic Visits")),
+         age = factor(age, levels= c("Age 0-4","Age 5-17","Age 18+","all")),
+         lag = factor(lag, levels= c("5-day","8-day","14-day")), 
+         sig = ifelse(U95 < 1 | L95 > 1, "Significant","Not Signficant"))%>%
+  filter(Disease %in% c("Respiratory (all)", "Cardiovascular (all)", "Total Encounters")) %>%
   ggplot() + 
-  geom_point(aes(x=lag, y=RR,  color=visit), position = position_dodge(width = 0.7)) + 
-  geom_errorbar(aes(x=lag, ymax=as.numeric(U95), ymin=as.numeric(L95), color=visit), position = position_dodge(width = 0.7)) + 
-  geom_hline(yintercept = 1, linetype="dashed") + facet_wrap(~ Disease) + coord_flip() 
+  geom_point(aes(x=lag, y=RR,  color=visit, shape=visit, alpha=sig), size=2,position = position_dodge(width = 0.7)) + 
+  geom_linerange(aes(x=lag, ymax=as.numeric(U95), ymin=as.numeric(L95), color=visit, alpha=sig), position = position_dodge(width = 0.7)) + 
+  geom_hline(yintercept = 1, linetype="dashed") + 
+  coord_cartesian(ylim=c(0.5, 2)) + facet_grid(.~ Disease)
+
+
++ coord_flip() 
 
 
 
+#####################################
+######### zip code tables
+#####################################
 
+install.packages("scales")
+library("scales")
+
+zipPM <- read.csv("./data/zipcodePM.csv", header=T, stringsAsFactors = F)%>%
+  mutate(date = as.Date(date, "%m/%d/%Y"))
+
+read.csv("./data/zipcodePM.csv", header=T, stringsAsFactors = F)  %>%
+  mutate(date = as.Date(date, "%m/%d/%Y")) %>%
+  select(zipcode, date, pm25) %>%
+  spread(key = date,value = pm25) %>%
+  write.csv(zipPM, "./data/zipPMspread.csv", row.names = F)
+
+avgPM <- zipPM %>%
+  group_by(date)%>%
+  summarise(average= mean(pm25)) %>%
+  filter(date >= as.Date("2007-10-11") & date < as.Date("2007-11-11"))
+
+
+
+zipPM %>%  
+  filter(date >= as.Date("2007-10-11") & date < as.Date("2007-11-11")) %>%
+  mutate(fiveDay = ifelse(date >= as.Date("2007-10-22") & date <= as.Date("2007-10-26"), pm25,""),
+         eightDay = ifelse(date >= as.Date("2007-10-22") & date <= as.Date("2007-10-29"), pm25,""),
+         fourteenDay = ifelse(date >= as.Date("2007-10-22") & date <= as.Date("2007-11-04"), pm25,"")) %>%
+  ggplot() + 
+  geom_point(aes(x=date, y=pm25,group=zipcode),color="gray90", alpha=0.25) + 
+  geom_line(data = avgPM, aes(x=date, y=average),  color="blue", size=1.4, alpha=0.4)+
+  geom_boxplot(aes(x=date, y=pm25,group=date),color="gray20", alpha=0.1) +
+  geom_hline(yintercept = 35, linetype="dashed", color="red", alpha=0.5, size=0.5) +
+  scale_x_date(labels = date_format("%m/%d")) +
+  theme(axis.text.x = element_text(angle=45))
+
+
+# write zip code averages
+zipPM %>%  
+  filter(date >= as.Date("2007-10-11") & date < as.Date("2007-11-11")) %>%
+  mutate(fiveDay = ifelse(date >= as.Date("2007-10-22") & date <= as.Date("2007-10-26"), pm25,"no"),
+         eightDay = ifelse(date >= as.Date("2007-10-22") & date <= as.Date("2007-10-29"), pm25,"no"),
+         fourteenDay = ifelse(date >= as.Date("2007-10-22") & date <= as.Date("2007-11-04"), pm25,"no")) %>%
+  gather(fiveDay, eightDay, fourteenDay, key = lag, value = pm25_period) %>%
+  filter(pm25_period != "no") %>%
+  group_by(lag) %>%
+  summarise(average = mean(as.numeric(pm25_period), na.rm=T), 
+            SD = sd(as.numeric(pm25_period),na.rm = T),
+            max = max(as.numeric(pm25_period), na.rm = T),
+            min = min(as.numeric(pm25_period), na.rm=T),
+            obs = length(as.numeric(pm25)),
+            per5th = quantile(as.numeric(pm25), probs=0.05),            
+            per25th = quantile(as.numeric(pm25), probs=0.25),
+            per50th = quantile(as.numeric(pm25), probs=0.5),
+            per75th = quantile(as.numeric(pm25), probs=0.75),
+            per95th = quantile(as.numeric(pm25), probs=0.95)) %>%
+  write.csv("./data/county_pm_stats.csv", row.names = F)
+
+
+# write daily stats
+zipPM %>%  
+  filter(date >= as.Date("2007-10-11") & date < as.Date("2007-11-11")) %>%
+  group_by(date) %>%
+  summarise(average = mean(as.numeric(pm25), na.rm=T), 
+            SD = sd(as.numeric(pm25),na.rm = T),
+            max = max(as.numeric(pm25), na.rm = T),
+            min = min(as.numeric(pm25), na.rm=T),
+            obs = length(as.numeric(pm25)),
+            per5th = quantile(as.numeric(pm25), probs=0.05),            
+            per25th = quantile(as.numeric(pm25), probs=0.25),
+            per50th = quantile(as.numeric(pm25), probs=0.5),
+            per75th = quantile(as.numeric(pm25), probs=0.75),
+            per85th = quantile(as.numeric(pm25), probs=0.85),
+            per95th = quantile(as.numeric(pm25), probs=0.95)) %>%
+  write.csv("./data/daily_pm_stats.csv", row.names = F)
+
+# box plot
+zipPM %>%  
+  filter(date >= as.Date("2007-10-11") & date < as.Date("2007-11-11")) %>%
+  ggplot() + geom_boxplot(aes(x=date, y=pm25,group=date),color="gray50", alpha=0.25) 
+  geom_hline(yintercept = 35, linetype="dashed", color="red", alpha=0.5, size=0.5) +
+  scale_x_date(labels = date_format("%m/%d")) +
+  theme(axis.text.x = element_text(angle=45))
+  
+# write zip code averages to map
+  zipPM %>%  
+    filter(date >= as.Date("2007-10-11") & date < as.Date("2007-11-11")) %>%
+    mutate(fiveDay = ifelse(date >= as.Date("2007-10-22") & date <= as.Date("2007-10-26"), pm25,"no"),
+           eightDay = ifelse(date >= as.Date("2007-10-22") & date <= as.Date("2007-10-29"), pm25,"no"),
+           fourteenDay = ifelse(date >= as.Date("2007-10-22") & date <= as.Date("2007-11-04"), pm25,"no")) %>%
+    gather(fiveDay, eightDay, fourteenDay, key = lag, value = pm25_period) %>%
+    filter(pm25_period != "no") %>%
+    group_by(zipcode, lag) %>%
+    summarise(average = mean(as.numeric(pm25_period), na.rm = T)) %>%
+    spread(key = lag, value = average)%>%
+    write.csv("./data/zip_code_pm_avg_4GIS_2.csv", row.names = F)
